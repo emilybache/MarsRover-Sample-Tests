608cf89820b4e726f3b831299c279e9a65b9fe2c
Emily Bache <emily@bacheconsulting.com>
2020-09-15 08:26:26 +0200

    Update sample2.cpp

--- cpp-catch2/test/sample2.cpp
+++ cpp-catch2/test/sample2.cpp
@@ -5,10 +5,11 @@
 
 TEST_CASE("receive single command should move backwards when command is B") {
     Coordinates start(1, 9);
-    Coordinates end(2, 9);
     std::vector<Obstacle> obstacles = {Coordinates(2,10)};
+    Plateau plateau(2,10);
+    plateau.setObstacles(obstacles);
 
-    MarsRover rover(start, end, N, obstacles);
+    MarsRover rover(plateau, N, start);
     std::vector<std::string> commands = {"B"};
     rover.acceptCommands(commands);
 
